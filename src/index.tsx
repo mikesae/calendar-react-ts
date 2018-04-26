1ad6459f7552d1d4f9db960b96fc722b878361ab
mikes <msaelens@gmail.com>
2018-04-26 09:45:19 -0400

    Remove separate storeProvider component, as workaround no longer needed.

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
-import StoreProvider from './storeProvider';
+
+import thunkMiddleware from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 render(
-    <StoreProvider>
+    <Provider store={store}>
         <App/>
-    </StoreProvider>,
+    </Provider>,
     document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
